beb5fac18c6850821fc158c126d9a538864774af
fazm1 <farid.azzam.2001@gmail.com>
2024-12-25 16:50:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function question1(arr1 =[]){
+    
+}
+
+function question2(sentence){
+    
+}
+
+function question3(number){
+
+}
+
+function question4(amount, coins = []){
+    
+}
+
+
+var q1 = question1();
+var q2 = question2();
+var q3 = question3();
+var q4 = question4();
